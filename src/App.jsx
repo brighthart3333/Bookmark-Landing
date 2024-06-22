e6d38c064c6798e57f602b985e41bd9019caf011
caleb <kalejaiyecaleb@gmail.com>
2024-06-23 00:36:47 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavMenu from "./components/NavMenu";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 const App = () => {
   return (
     <div className="lg:px-[100px]">
       <NavMenu />
       <Hero />
+      <Features />
     </div>
   );
 };
---
+++ src/components/Features.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="mt-[30px] text-center justify-center m-auto">
+      <h1 className="text-[20px] text-bold">Features</h1>
+      <p className="text-[--Grayish-Blue] text-[12px] mt-[5px] w-[350px] text-center m-auto">
+        Our aim is to make it quick and easy for you to access your favorite
+        websites. Your bookmarks sync between your devices so you can access
+        them on the go.
+      </p>
+    </section>
+  );
+};
+
+export default Features;
